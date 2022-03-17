819e1b268d174379a69876159d3e4728fb58d946
PhuongHX <xuanphuong3105@gmail.com>
2022-03-17 17:07:21 +0700

    Added template

---
+++ app/imports/ui/components/filter/filter.js
@@ -0,0 +1,17 @@
+import './filter.html';
+
+Template.filter.onCreated(() => {
+
+});
+
+Template.filter.onRendered(() => {
+
+});
+
+Template.filter.helpers({
+
+});
+
+Template.filter.events({
+
+});
---
+++ app/imports/ui/components/tpl/tpl.js
@@ -0,0 +1,17 @@
+import './tpl.html';
+
+Template.tpl.onCreated(() => {
+
+});
+
+Template.tpl.onRendered(() => {
+
+});
+
+Template.tpl.helpers({
+
+});
+
+Template.tpl.events({
+    
+});
