ccca455a1c48cd0d7c40e531640f6f7900dfbbf8
PhuongHX <xuanphuong3105@gmail.com>
2022-03-21 15:49:56 +0700

    Added filter

--- app/imports/ui/components/filter/filter.js
+++ app/imports/ui/components/filter/filter.js
@@ -1,11 +1,31 @@
 import './filter.html';
+import * as noUiSlider from 'nouislider';
+import 'nouislider/dist/nouislider.css';
 
 Template.filter.onCreated(() => {
 
 });
 
 Template.filter.onRendered(() => {
+    const magnitudeSlider = document.getElementById('magnitude-slider');
+    const depthSlider = document.getElementById('depth-slider');
+    noUiSlider.create(magnitudeSlider, {
+        start: [20, 80],
+        connect: true,
+        range: {
+            'min': 0,
+            'max': 100
+        }
+    });
 
+    noUiSlider.create(depthSlider, {
+        start: [20, 80],
+        connect: true,
+        range: {
+            'min': 0,
+            'max': 100
+        }
+    });
 });
 
 Template.filter.helpers({
