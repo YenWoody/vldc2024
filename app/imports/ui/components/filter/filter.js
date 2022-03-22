6508abffef27a1fca7a13e59663bfda6366be5af
PhuongHX <xuanphuong3105@gmail.com>
2022-03-22 09:47:36 +0700

    Date picker

--- app/imports/ui/components/filter/filter.js
+++ app/imports/ui/components/filter/filter.js
@@ -1,6 +1,11 @@
 import './filter.html';
+// nouislider
 import * as noUiSlider from 'nouislider';
+import wNumb from 'wnumb';
 import 'nouislider/dist/nouislider.css';
+// Litepicker
+import Litepicker from 'litepicker';
+import 'litepicker/dist/css/litepicker.css';
 
 Template.filter.onCreated(() => {
 
@@ -12,19 +17,41 @@ Template.filter.onRendered(() => {
     noUiSlider.create(magnitudeSlider, {
         start: [20, 80],
         connect: true,
+        tooltips: true,
+        step: 1,
+        behaviour: 'tap-drag',
         range: {
             'min': 0,
             'max': 100
-        }
+        },
+        format: wNumb({
+            decimals: 0,
+        })
     });
 
     noUiSlider.create(depthSlider, {
         start: [20, 80],
         connect: true,
+        tooltips: true,
+        step: 1,
+        behaviour: 'tap-drag',
         range: {
             'min': 0,
             'max': 100
-        }
+        },
+        format: wNumb({
+            decimals: 0,
+        })
+    });
+
+    const datePicker = new Litepicker({
+        element: document.getElementById('start-date'),
+        elementEnd: document.getElementById('end-date'),
+        // singleMode: false,
+        // inlineMode: true,
+        // disallowLockDaysInRange: true,
+        singleMode: false,
+        allowRepick: true,
     });
 });
 
