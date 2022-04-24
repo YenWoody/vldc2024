f8f8bdf2f0ee379165e040824766d93722d58f7d
YenWoody <yenph@fimo.edu.vn>
2022-04-24 23:09:11 +0700

    dashboard,upload

--- app/imports/ui/components/dashboard/dashboard.js
+++ app/imports/ui/components/dashboard/dashboard.js
@@ -1,7 +1,8 @@
 import './dashboard.html';
 import { Template } from 'meteor/templating';
 import '../../pages/login/login';
-import 'jquery';
+import { Meteor } from 'meteor/meteor';
+import { $ } from 'meteor/jquery';
 const getUser = () => Meteor.user();
 const isUserLogged = () => !!getUser();
 Template.dashboard.helpers({
@@ -10,22 +11,82 @@ Template.dashboard.helpers({
     },
     getUser() {
         return getUser();
-    }
+    },
+
+  });
+  Meteor.startup( function () {   
+    $.getScript("/plugins/js/jquery.sparkline.min.js");
+    
   });
 Template.dashboard.onRendered(function() {
-    this.autorun(() => {
-        if (isUserLogged()) {
-            FlowRouter.go('/dashboard');
-        }
-    });
-    $('#sparklinedash').sparkline([102,109,120,99,110,80,87,74,102,109,120,99,110,80,87,74], {
+  
+    setTimeout(function(){
+    $('#sparklinedash').sparkline([102,109,120,99,110,80,87,74], {
         type: 'bar',
-        height: '100',
+        height: '30',
         barWidth: 9,
         barSpacing: 10,
         barColor: '#177dff'
-        });
+    });},2000);
+    setTimeout(function(){
+        $('#sparklinedash1').sparkline([102,109,120,99,110,80,87,74], {
+            type: 'bar',
+            height: '30',
+            barWidth: 9,
+            barSpacing: 10,
+            barColor: '#177dff'
+        });},2000);
+    setTimeout(function(){
+            $('#sparklinedash2').sparkline([102,109,120,99,110,80,87,74], {
+                type: 'bar',
+                height: '30',
+                barWidth: 9,
+                barSpacing: 10,
+                barColor: '#177dff'
+            });},2000);
+
+
 });
 Template.dashboard.onCreated(function() {
+   
+    $(function() {
+        "use strict";
+    
+        $(".preloader").fadeOut();
+        // this is for close icon when navigation open in mobile view
+        $(".nav-toggler").on('click', function() {
+            $("#main-wrapper").toggleClass("show-sidebar");
+            $(".nav-toggler i").toggleClass("ti-menu");
+        });
+        $(".search-box a, .search-box .app-search .srh-btn").on('click', function() {
+            $(".app-search").toggle(200);
+            $(".app-search input").focus();
+        });
+    
+        // ============================================================== 
+        // Resize all elements
+        // ============================================================== 
+        $("body, .page-wrapper").trigger("resize");
+        $(".page-wrapper").delay(20).show();
+        
+        //****************************
+        /* This is for the mini-sidebar if width is less then 1170*/
+        //**************************** 
+        var setsidebartype = function() {
+            var width = (window.innerWidth > 0) ? window.innerWidth : this.screen.width;
+            if (width < 1170) {
+                $("#main-wrapper").attr("data-sidebartype", "mini-sidebar");
+            } else {
+                $("#main-wrapper").attr("data-sidebartype", "full");
+            }
+        };
+        $(window).ready(setsidebartype);
+        $(window).on("resize", setsidebartype);
+    
+    });
+
+});
+Template.dashboard.onRendered(function() {
+
 
 })
